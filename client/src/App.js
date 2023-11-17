4f1801fe8fadb6e272644fa76f00dcb941cadb35
SamuliCas <samuli.castren@gmail.com>
2023-11-17 12:10:27 +0200

    Added comments

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, NavDropdown, Container, Form, Button } from 'react-bootstrap';
 
-
 function App() {
+  //State to store data fethced from backend
   const [backendData, setBackendData] = useState([{}])
 
+  // States to handle username input and password input
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
+  //Fetch data from the backend
   useEffect(() => {
     fetch("/users")
       .then(response => response.json())
@@ -16,6 +21,27 @@ function App() {
         console.error("Error fetching data:", error);
       });
   }, []);
+
+  // Handle the registration when the "Register" button is clicked
+  const handleLogin = () => {
+    fetch("/addUser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error("Error adding user:", error);
+      });
+      // Reload the page after registration
+      window.location.reload();
+  };
+
 
   return (
     <div>
@@ -35,6 +61,35 @@ function App() {
         </Container>
       </Navbar>
 
+      <Container>
+        <Form>
+          <Form.Group controlId="formUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </Form.Group>
+
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Enter password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Group>
+
+          <Button variant="primary" onClick={handleLogin}>
+            Register
+          </Button>
+        </Form>
+      </Container>
+
+      {/* Just simple way to display backend data */}
       {backendData.length === 0 ? (
         <p>Loading...</p>
       ) : (
--- server/server.js
+++ server/server.js
@@ -4,7 +4,9 @@ const cors = require('cors')
 
 const app = express()
 app.use(cors())
+app.use(express.json()); 
 
+// Create a MySQL database connection
 const db = mysql.createConnection({
     host: "localhost",
     user: 'root',
@@ -12,6 +14,7 @@ const db = mysql.createConnection({
     database: 'mydb'
 })
 
+// Define a route to fetch all users from the database
 app.get('/users', (req, res)=> {
     const sql = "SELECT * FROM users";
     db.query(sql, (err, data)=> {
@@ -20,8 +23,29 @@ app.get('/users', (req, res)=> {
     })
 })
 
-app.get("/api", (req, res) =>{
-    res.json({"users": ["userOne", "userTwo", "userThree"] })
-})
+// Define a route to add a new user to the database
+app.post('/addUser', (req, res) => {
+    const { username, password } = req.body;
+
+    // Validate that both username and password are provided  
+    if (!username || !password) {
+      return res.status(400).json({ error: "Username and password are required." });
+    }
+  
+    const sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+    db.query(sql, [username, password], (err, result) => {
+      if (err) {
+        console.error("Error adding user to the database:", err);
+        return res.status(500).json({ error: "Internal server error." });
+      }
+      
+      // Respond with an empty JSON object (success)  
+      res.json({});
+    });
+  });
+
+// app.get("/api", (req, res) =>{
+//     res.json({"users": ["userOne", "userTwo", "userThree"] })
+// })
 
 app.listen(5000, () => { console.log("Server started on port 5000") })
